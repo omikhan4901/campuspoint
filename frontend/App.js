878e9890b08c81a72d87086c18ee8a2709102ddb
Omi Khan <64612072+Pandaaa-dev@users.noreply.github.com>
2024-08-28 23:16:38 +0600

    Added Design file v1.0.0

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>WazzuPP</Text>
+      <Text>WE THE BEST MUSIC</Text>
       <StatusBar style="dark" />
     </View>
   );
